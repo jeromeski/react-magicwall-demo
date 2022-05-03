0ba77201f0518fbba8348dbe81a002cbfe640880
jeromeski <jeromegee@gmail.com>
2022-05-03 21:12:58 +0800

    refactor 4

--- src/components/MagicWall.js
+++ src/components/MagicWall.js
@@ -1,12 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
-import mono from "../assets/wp-content/uploads/2021/11/MONO-Tlumaczenia-ktore-uwalniaja-mozliwosci-o-nas-Biuro-tlumaczen-MONO-Torun.png";
-import club from "../assets/wp-content/uploads/2021/11/https-club-silversurfers-com-wp-content-themes-club-2B-img-logo-svg.png";
-import pobrane3 from "../assets/wp-content/uploads/2021/11/pobrane-3.jpg";
-import pobrane1 from "../assets/wp-content/uploads/2021/11/pobrane-1.jpg";
-import jane from "../assets/wp-content/uploads/2021/11/Home-Jane-Anson-Inside-Bordeaux-janeanson-com.png";
-import pobrane from "../assets/wp-content/uploads/2021/11/pobrane.jpg";
-import pobrane2 from "../assets/wp-content/uploads/2021/11/pobrane-2.jpg";
+import { mono, club, pobrane3, pobrane1, jane, pobrane, pobrane2 } from "../data/index";
 
 function MagicWall() {
 	return (
---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+import mono from "../assets/wp-content/uploads/2021/11/MONO-Tlumaczenia-ktore-uwalniaja-mozliwosci-o-nas-Biuro-tlumaczen-MONO-Torun.png";
+import club from "../assets/wp-content/uploads/2021/11/https-club-silversurfers-com-wp-content-themes-club-2B-img-logo-svg.png";
+import pobrane3 from "../assets/wp-content/uploads/2021/11/pobrane-3.jpg";
+import pobrane1 from "../assets/wp-content/uploads/2021/11/pobrane-1.jpg";
+import jane from "../assets/wp-content/uploads/2021/11/Home-Jane-Anson-Inside-Bordeaux-janeanson-com.png";
+import pobrane from "../assets/wp-content/uploads/2021/11/pobrane.jpg";
+import pobrane2 from "../assets/wp-content/uploads/2021/11/pobrane-2.jpg";
+
+export { mono, club, pobrane3, pobrane1, jane, pobrane, pobrane2 };
